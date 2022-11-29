d04351243d7aeff3139cbd7a4903472b80fd0f52
Pap. I. FY <=>
2022-11-29 12:56:21 +0000

    Added test package

---
+++ src/test/java/com/niit/vay/VayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niit.vay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
